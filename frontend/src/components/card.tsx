b6845155bc89b88603f71b67ab519ddd220c109c
Cindy Halim <jcindyhalim@gmail.com>
2021-11-10 03:32:17 -0500

    add different sizes to card component, build card empty state component

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,22 +1,25 @@
-import React from "react"
-import { Flex, Image, Text } from "rebass"
-import { theme } from "../theme"
-import { CardSymbol } from "../types"
+import React from "react";
+import { Flex, Image, SxStyleProp, Text } from "rebass";
+import { theme } from "../theme";
+import { CardSymbol } from "../types";
 
-import Circle from "src/assets/circle.svg"
-import Diamond from "src/assets/diamond.svg"
-import Dots from "src/assets/dots.svg"
-import Hash from "src/assets/hash.svg"
-import Pause from "src/assets/pause.svg"
-import Plus from "src/assets/plus.svg"
-import Star from "src/assets/star.svg"
-import Wave from "src/assets/wave.svg"
-import CardBackground from "src/assets/card-background.svg"
+import Circle from "src/assets/circle.svg";
+import Diamond from "src/assets/diamond.svg";
+import Dots from "src/assets/dots.svg";
+import Hash from "src/assets/hash.svg";
+import Pause from "src/assets/pause.svg";
+import Plus from "src/assets/plus.svg";
+import Star from "src/assets/star.svg";
+import Wave from "src/assets/wave.svg";
+import CardBackground from "src/assets/card-background.svg";
 
+export type TCardSize = "small" | "medium" | "large";
 interface ICardProps {
-  symbol: CardSymbol
-  text: string
-  side?: "front" | "back"
+  symbol: CardSymbol;
+  text: string;
+  side?: "front" | "back";
+  size?: TCardSize;
+  containerSx?: SxStyleProp;
 }
 
 const symbolToImg: { [key in CardSymbol]: string } = {
@@ -28,7 +31,7 @@ const symbolToImg: { [key in CardSymbol]: string } = {
   PLUS: Plus,
   STAR: Star,
   WAVE: Wave,
-}
+};
 
 const CARD_COLORS = [
   theme.colors.black,
@@ -36,14 +39,36 @@ const CARD_COLORS = [
   theme.colors.orange,
   theme.colors.red,
   theme.colors.sand,
-]
+];
+
+export const getStylesFromSize = (size: TCardSize) => {
+  switch (size) {
+    case "large":
+      return theme.styles.largeCard;
+    case "medium":
+      return theme.styles.mediumCard;
+    case "small":
+    default:
+      return theme.styles.smallCard;
+  }
+};
 
 export const Card: React.FC<ICardProps> = ({
   symbol,
   text,
   side = "front",
+  size = "small",
+  containerSx,
 }) => {
-  const color = CARD_COLORS[Math.floor(Math.random() * CARD_COLORS.length)]
+  const {
+    containerCard: containerCardStyles,
+    innerCard: innerCardStyles,
+    symbol: symbolStyles,
+    fontSize,
+  } = getStylesFromSize(size);
+
+  const color = CARD_COLORS[Math.floor(Math.random() * CARD_COLORS.length)];
+
   return (
     <Flex
       sx={{
@@ -51,37 +76,40 @@ export const Card: React.FC<ICardProps> = ({
         backgroundColor: theme.colors.white,
         borderRadius: 10,
         filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
-        width: 242,
-        height: 327,
         justifyContent: "center",
         alignItems: "center",
         border: `2px solid ${side === "front" ? theme.colors.white : color}`,
+        ...containerCardStyles,
+        ...containerSx,
       }}
     >
       {side === "front" ? (
         <>
-          <CardText>{text}</CardText>
+          <CardText fontSize={fontSize.front}>{text}</CardText>
           <Image
-            sx={{ height: 75, width: 75, marginY: 50, fill: "pink" }}
+            sx={{ ...symbolStyles, fill: "pink" }}
             src={symbolToImg[symbol]}
           />
-          <CardText upsideDown>{text}</CardText>
+          <CardText fontSize={fontSize.front} upsideDown>
+            {text}
+          </CardText>
         </>
       ) : (
         <Flex
           sx={{
             backgroundColor: color,
-            width: 228,
-            height: 307,
             justifyContent: "center",
             alignItems: "center",
-            borderRadius: 15,
+            ...innerCardStyles,
           }}
         >
-          <Image src={CardBackground} sx={{ position: "absolute" }} />
+          <Image
+            src={CardBackground}
+            sx={{ position: "absolute", height: "85%", width: "85%" }}
+          />
           <Text
             sx={{
-              fontSize: 45,
+              fontSize: fontSize.back,
               fontFamily: theme.fonts.sancreek,
               color: theme.colors.white,
             }}
@@ -91,25 +119,32 @@ export const Card: React.FC<ICardProps> = ({
         </Flex>
       )}
     </Flex>
-  )
-}
+  );
+};
 
 interface ICardTextProps {
-  upsideDown?: boolean
+  upsideDown?: boolean;
+  fontSize?: number;
 }
 
-const CardText: React.FC<ICardTextProps> = ({ children, upsideDown }) => {
+const CardText: React.FC<ICardTextProps> = ({
+  children,
+  upsideDown,
+  fontSize,
+}) => {
   return (
     <Text
       sx={{
         fontFamily: theme.fonts.antonio,
-        fontSize: 45,
         color: theme.colors.black,
         textTransform: "uppercase",
+        overflowWrap: "break-word",
+        textAlign: "center",
+        fontSize,
         ...(upsideDown && { transform: "rotate(180deg)" }),
       }}
     >
       {children}
     </Text>
-  )
-}
+  );
+};
