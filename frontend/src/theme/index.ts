9802e330c1f23aabdab364fd023acea6b953578d
Cindy Halim <jcindyhalim@gmail.com>
2021-11-10 03:32:51 -0500

    add card size styling to common themes

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -6,9 +6,30 @@ export const theme = {
     orange: "#F78C5A",
     navy: "#18414B",
     sand: "#D99E7F",
+    lightBg: "#F8F7F7",
   },
   fonts: {
     sancreek: "Sancreek",
     antonio: "Antonio",
   },
-}
+  styles: {
+    largeCard: {
+      containerCard: { width: 242, height: 327 },
+      innerCard: { width: 228, height: 307, borderRadius: 15 },
+      symbol: { width: 75, height: 75, marginY: 50 },
+      fontSize: { back: 45, front: 45 },
+    },
+    mediumCard: {
+      containerCard: { width: 180, height: 245 },
+      innerCard: { width: 165, height: 230, borderRadius: 10 },
+      symbol: { width: 30, height: 30, marginY: 30 },
+      fontSize: { back: 34, front: 34 },
+    },
+    smallCard: {
+      containerCard: { width: 140, height: 185 },
+      innerCard: { width: 128, height: 172, borderRadius: 10 },
+      symbol: { width: 30, height: 30, marginY: 30 },
+      fontSize: { back: 26, front: 20 },
+    },
+  },
+};
