dda5cd649cdc387b5c4a12d407de0c1e06eae51f
Cindy Halim <52426595+cindyhalim@users.noreply.github.com>
2022-03-26 14:43:38 -0400

    Game screen (#4)
    * setup iamrolestatements, websocket endpoint, dynamodb table resource
    
    * setup s3 resource, clean up functions template, types for iamrolestatements per function
    
    * removing .serverless
    
    * add utils for dynamodb, s3, ws; cleanup directories
    
    * type websockets send message body, add onConnect handler, clean up functions file
    
    * make shared types, type api gateway event, add onDisconnect handler
    
    * update database models, update onDisconnect, add serializer, update shared types
    
    * update onDisconnect handler source
    
    * add different sizes to card component, build card empty state component
    
    * add card size styling to common themes
    
    * build game room screen, modify reducer and action based on backend changes
    
    * prettier be bugging and adding semicolons
    
    * hide overflowing card
    
    * fix safari ui issue
    
    * feat: add game difficulty type
    
    * chore: resolve merge conflicts, use shared types
    
    * fix: return game status in getSerializedRoom
    
    * chore: remove unused actions and reducer

---
+++ frontend/src/components/card-empty-state.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Box } from "rebass"
+
+import { getStylesFromSize, TCardSize } from "./card"
+
+interface ICardEmptyStateProps {
+  size: TCardSize
+}
+export const CardEmptyState: React.FC<ICardEmptyStateProps> = ({ size }) => {
+  const { containerCard } = getStylesFromSize(size)
+  return (
+    <Box
+      sx={{
+        width: containerCard.width,
+        height: containerCard.height,
+        bg: "grey",
+        opacity: 0.3,
+        borderRadius: 15,
+      }}
+    />
+  )
+}
---
+++ frontend/src/features/game/game-room.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { Box, Flex } from "rebass"
+import { CardEmptyState } from "src/components/card-empty-state"
+
+import { useAppSelector } from "src/redux/utils"
+import { theme } from "src/theme"
+import { CardSymbol } from "src/types"
+import { Card } from "../../components/card"
+import { PlayerBlock } from "./player-block"
+
+export const GameRoom: React.FC = () => {
+  const players = useAppSelector((state) => state.players)
+  const playerId = useAppSelector((state) => state.playerId)
+
+  const opponentPlayers = players.filter((player) => player.id !== playerId)
+  const player = players.find((player) => player.id === playerId)
+
+  return (
+    <Box
+      sx={{
+        bg: theme.colors.lightBg,
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        overflowY: "hidden",
+        paddingX: 10,
+        paddingTop: 15,
+      }}
+    >
+      <Flex
+        justifyContent={"space-evenly"}
+        sx={{
+          width: "100%",
+          minHeight: theme.styles.smallCard.containerCard.height,
+        }}
+      >
+        {opponentPlayers.map((opponentPlayer) => (
+          <PlayerBlock
+            key={opponentPlayer.id}
+            player={opponentPlayer}
+            isMe={false}
+          />
+        ))}
+      </Flex>
+      <Flex sx={{ justifyContent: "center", marginTop: 87 }}>
+        <Card
+          text={"DRAW PILE"}
+          symbol={CardSymbol.DIAMOND}
+          size={"medium"}
+          side={"back"}
+          containerSx={{ marginRight: 32 }}
+        />
+        <CardEmptyState size={"medium"} />
+      </Flex>
+      <PlayerBlock player={player} isMe={true} />
+    </Box>
+  )
+}
