141be3270bde8f2cec3705a9e2c2e35b252f4bf7
Cindy Halim <52426595+cindyhalim@users.noreply.github.com>
2022-03-29 20:07:18 -0400

    fix: shared package porblem (#8)
    * fix: update package configs
    
    * fix: addtemp card bucket key, return status code in onConnect handler
    
    * chore: remove lib from .gitignore

--- backend/src/functions/onConnect.ts
+++ backend/src/functions/onConnect.ts
@@ -1,16 +1,16 @@
-import { APIGatewayEvent } from "aws-lambda";
+import { APIGatewayEvent } from "aws-lambda"
 
-import { ws } from "@services";
-import { middyfy } from "@utils";
+import { ws } from "@services"
+import { middyfy } from "@utils"
 
-import { IPlayerIdSetEvent, BackendWebsocketActions } from "alacrity-shared";
+import { IPlayerIdSetEvent, BackendWebsocketActions } from "alacrity-shared"
 
 const onConnect = async (event: APIGatewayEvent) => {
   const {
     requestContext: { routeKey, connectionId },
-  } = event;
-  console.log("onConnect: recieved route key:", routeKey);
-  console.log("connectionId:", connectionId);
+  } = event
+  console.log("onConnect: recieved route key:", routeKey)
+  console.log("connectionId:", connectionId)
 
   await ws.sendMessage<IPlayerIdSetEvent>({
     connectionId,
@@ -18,7 +18,9 @@ const onConnect = async (event: APIGatewayEvent) => {
       action: BackendWebsocketActions.PlayerIdSet,
       playerId: connectionId,
     },
-  });
-};
+  })
 
-export const handler = middyfy(onConnect);
+  return { statusCode: 200 }
+}
+
+export const handler = middyfy(onConnect)
