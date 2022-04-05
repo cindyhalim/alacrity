fc7049cff3d199ecb7175acba12befaa0700c4d7
Cindy Halim <jcindyhalim@gmail.com>
2022-04-04 22:45:35 -0400

    fix: deleted too much

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,16 @@ import React from "react"
 import { WaitingRoom, GameRoom } from "./features"
 import { useAppSelector } from "./redux/utils"
 import { LoadingTitleScreen } from "./components/loading-title-screen"
+import { ErrorScreen } from "./features/error/error-screen"
 
 export const App: React.FC = () => {
   const gameStatus = useAppSelector((state) => state?.currentGame?.status)
+  const roomStatus = useAppSelector((state) => state.roomStatus)
   const playerId = useAppSelector((state) => state.playerId)
+
+  if (roomStatus !== "ready") {
+    return <ErrorScreen />
+  }
 
   if (!playerId) {
     return <LoadingTitleScreen />
