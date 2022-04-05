f95690cc8247e85c107217bb4416696272209169
Emily Dong <38989901+emilyhfdong@users.noreply.github.com>
2022-04-05 09:23:42 -0400

    4 - loading state (#13)
    * chore: use lottie animation for loading screen
    
    * fix: fix styling on waiting room
    
    * chore: remove difficulty

--- frontend/src/components/loading-title-screen.tsx
+++ frontend/src/components/loading-title-screen.tsx
@@ -1,6 +1,26 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { BaseTitleScreen } from "./base-title-screen"
+import loadingAnimation from "../assets/loading.json"
+import lottie, { AnimationItem } from "lottie-web"
+import { Box } from "rebass"
 
 export const LoadingTitleScreen: React.FC = () => {
-  return <BaseTitleScreen>LOADING</BaseTitleScreen>
+  const element = useRef<HTMLDivElement>(null)
+  const lottieInstance = useRef<AnimationItem>()
+
+  useEffect(() => {
+    if (element.current) {
+      lottieInstance.current = lottie.loadAnimation({
+        animationData: loadingAnimation,
+        container: element.current,
+      })
+      lottieInstance.current.setSpeed(3)
+    }
+  }, [])
+
+  return (
+    <BaseTitleScreen sx={{ justifyContent: "center" }}>
+      <Box sx={{ width: 100, height: 100 }} ref={element}></Box>
+    </BaseTitleScreen>
+  )
 }
--- frontend/src/features/waiting-room.tsx
+++ frontend/src/features/waiting-room.tsx
@@ -1,71 +1,49 @@
-import React, { useState } from "react"
+import React from "react"
 import { Flex, Text } from "rebass"
 import { Button } from "src/components"
 import { useAppSelector } from "src/redux/utils"
 import { theme } from "src/theme"
-import { GameDifficulty } from "alacrity-shared"
 import { BaseTitleScreen } from "src/components/base-title-screen"
 import { getIsAdmin } from "src/utils/helpers"
 
 export const WaitingRoom: React.FC = () => {
-  const [selectedDifficulty, setSelectedDifficulty] = useState<GameDifficulty>(
-    GameDifficulty.MEDIUM,
-  )
   const players = useAppSelector((state) => state.playerPool)
   const roomId = useAppSelector((state) => state.roomId)
   const playerId = useAppSelector((state) => state.playerId)
   return (
-    <BaseTitleScreen>
-      <Text
-        sx={{
-          fontFamily: theme.fonts.antonio,
-          fontSize: 28,
-          marginBottom: 20,
-        }}
-      >
-        DIFFICULTY
-      </Text>
-      <Flex sx={{ marginBottom: 30 }}>
-        {Object.values(GameDifficulty).map((difficulty, idx) => (
-          <Button
-            key={idx}
-            active={selectedDifficulty === difficulty}
-            sx={{ marginLeft: idx === 0 ? 0 : 20 }}
-            onClick={() => setSelectedDifficulty(difficulty)}
-          >
-            {difficulty.replace("_", " ")}
-          </Button>
-        ))}
+    <BaseTitleScreen sx={{ justifyContent: "space-between" }}>
+      <Flex sx={{ flexDirection: "column", alignItems: "center" }}>
+        <Text
+          sx={{
+            fontFamily: theme.fonts.antonio,
+            fontSize: 28,
+            marginBottom: 20,
+          }}
+        >
+          PLAYERS
+        </Text>
+        <Flex sx={{ flexWrap: "wrap" }}>
+          {players.map((player, idx) => (
+            <Button
+              active={player.id === playerId}
+              key={idx}
+              sx={{ marginLeft: idx === 0 ? 0 : 20, marginBottom: 20 }}
+            >
+              {player.name.toUpperCase()}
+            </Button>
+          ))}
+        </Flex>
       </Flex>
-      <Text
-        sx={{
-          fontFamily: theme.fonts.antonio,
-          fontSize: 28,
-          marginBottom: 20,
-        }}
-      >
-        PLAYERS
-      </Text>
-      <Flex sx={{ flexWrap: "wrap" }}>
-        {players.map((player, idx) => (
-          <Button
-            active={player.id === playerId}
-            key={idx}
-            sx={{ marginLeft: idx === 0 ? 0 : 20, marginBottom: 20 }}
-          >
-            {player.name.toUpperCase()}
-          </Button>
-        ))}
-      </Flex>
+
       {getIsAdmin() && (
         <Flex sx={{ marginTop: 60, width: "100%" }}>
           <Button
             onClick={() => navigator.clipboard.writeText(window.location.href + roomId)}
-            sx={{ flex: 1 }}
+            sx={{ flex: 1, fontSize: 20 }}
           >
             COPY LINK
           </Button>
-          <Button disabled={players.length > 4} sx={{ flex: 1, marginLeft: 20 }}>
+          <Button disabled={players.length > 4} sx={{ flex: 1, marginLeft: 20, fontSize: 20 }}>
             START NEW GAME
           </Button>
         </Flex>
