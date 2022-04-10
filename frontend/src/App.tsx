012ce4cd42c298811069de1b2ef918bda28a91c3
Cindy Halim <jcindyhalim@gmail.com>
2022-04-10 01:28:25 -0400

    feat: show score board logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { WaitingRoom, GameRoom } from "./features"
 import { useAppSelector } from "./redux/utils"
 import { LoadingTitleScreen } from "./components/loading-title-screen"
 import { ErrorScreen } from "./features/error/error-screen"
+import { ScoreBoard } from "./features/score-board"
 
 export const App: React.FC = () => {
   const currentGame = useAppSelector((state) => state?.currentGame)
   const roomStatus = useAppSelector((state) => state.roomStatus)
   const playerId = useAppSelector((state) => state.playerId)
+  const gameScore = useAppSelector((state) => state.gameScore)
 
   if (roomStatus !== "ready") {
     return <ErrorScreen />
@@ -15,6 +17,10 @@ export const App: React.FC = () => {
 
   if (!playerId) {
     return <LoadingTitleScreen />
+  }
+
+  if (gameScore && gameScore.length) {
+    return <ScoreBoard />
   }
 
   return !currentGame ? <WaitingRoom /> : <GameRoom />
