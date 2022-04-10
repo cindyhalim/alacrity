cbcc5eede2e29f782770721f3a8fa663ad226ba8
Cindy Halim <jcindyhalim@gmail.com>
2022-04-09 21:17:09 -0400

    feat: handle card win logic

--- frontend/src/features/game/player-block.tsx
+++ frontend/src/features/game/player-block.tsx
@@ -1,8 +1,10 @@
-import { IGamePlayer } from "alacrity-shared"
+import { FrontendWebsocketActions, IGamePlayer, IPlayingCard, IWildCard } from "alacrity-shared"
 import React from "react"
 import { Flex } from "rebass"
 import { PlayerCard, IPlayerCardProps } from "src/components"
+import { useAppSelector } from "src/redux/utils"
 import { useGetMainPlayer } from "src/utils/helpers"
+import { useWSContext } from "src/utils/websocket-context"
 
 interface IPlayerBlockProps {
   player: IGamePlayer
@@ -11,6 +13,8 @@ interface IPlayerBlockProps {
 
 export const PlayerBlock: React.FC<IPlayerBlockProps> = ({ player, isCurrentPlayerTurn }) => {
   const mainPlayer = useGetMainPlayer()
+  const { sendMessage } = useWSContext()
+  const wildCard = useAppSelector((state) => state.currentGame?.wildCard)
 
   const playPile = player.playPile
   const playerCard = playPile?.[playPile.length - 1] || null
@@ -24,8 +28,39 @@ export const PlayerBlock: React.FC<IPlayerBlockProps> = ({ player, isCurrentPlay
     isCurrentPlayerTurn,
   }
 
+  const handleCardClick = ({
+    playerCard,
+    mainPlayer,
+    wildCard,
+  }: {
+    playerCard: IPlayingCard | null
+    mainPlayer: IGamePlayer | undefined
+    wildCard: IWildCard | undefined
+  }) => {
+    const mainPlayerPile = mainPlayer?.playPile
+    const mainPlayerCard = mainPlayerPile?.[mainPlayerPile.length - 1] || null
+
+    const matchesWildCard = wildCard?.symbols.every(
+      (symbol) => symbol === mainPlayerCard?.symbol || symbol === playerCard?.symbol,
+    )
+
+    if (!playerCard) {
+      return
+    }
+
+    if (mainPlayerCard?.symbol === playerCard.symbol || matchesWildCard) {
+      sendMessage({
+        action: FrontendWebsocketActions.CardWon,
+        loserPlayerId: player.id,
+      })
+    }
+  }
+
   return isOpponent ? (
-    <PlayerCard {...sharedProps} />
+    <PlayerCard
+      {...sharedProps}
+      onClick={() => handleCardClick({ playerCard, mainPlayer, wildCard })}
+    />
   ) : (
     <Flex
       sx={{
