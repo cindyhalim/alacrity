9756a37d72250617185ec88c8fc698077ef0065e
Cindy Halim <jcindyhalim@gmail.com>
2022-04-12 20:59:58 -0400

    chore: actually rename hook lol

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -2,7 +2,7 @@ import { useAppSelector } from "src/redux/utils"
 
 export const getIsAdmin = () => !window.location.pathname.slice(1)
 
-export const useGetMainPlayer = () => {
+export const useMainPlayer = () => {
   const players = useAppSelector((state) => state.currentGame?.players || [])
   const playerId = useAppSelector((state) => state.playerId)
   const mainPlayer = players.find((player) => player.id === playerId)
