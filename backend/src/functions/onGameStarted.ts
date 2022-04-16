0e8e4d4baac8606fcf4b66f69a50ffd1636b2cc1
Cindy Halim <52426595+cindyhalim@users.noreply.github.com>
2022-04-16 16:43:30 -0400

    fix: delete previous game on game started event (#26)
    * fix: delete prevous game on game started event
    
    * fix: remove any previous game

--- backend/src/functions/onGameStarted.ts
+++ backend/src/functions/onGameStarted.ts
@@ -2,7 +2,7 @@ import { nanoid } from "nanoid"
 import { BackendWebsocketActions, IGameStartedEvent, IGameUpdatedEvent } from "alacrity-shared"
 
 import { cardsData, database, IGameModel, IGamePlayer, ws } from "@services"
-import { getPlayers } from "@utils"
+import { getGame, getPlayers } from "@utils"
 import { APIGatewayEvent } from "aws-lambda"
 import { getDrawPile, shuffle } from "../helpers/cards"
 
@@ -22,6 +22,12 @@ export const handler = async (event: APIGatewayEvent) => {
 
   console.log("getting game players")
   const room = await database.room.get({ roomId })
+  const game = getGame(room)
+
+  if (game) {
+    await database.room.deleteGame({ roomId, gameId: game.id })
+  }
+
   const rawPlayers = getPlayers(room)
   const randomOrderedPlayers = shuffle(rawPlayers)
   const players: IGamePlayer[] = randomOrderedPlayers.map((currPlayer) => ({
